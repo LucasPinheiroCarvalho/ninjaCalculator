3b95607192570408a131aa4d5606656ed3bd64b6
Groove Tech <lucas.pinho.31@gmail.com>
2021-12-10 10:29:53 -0300

    :new: aula 2

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 function component() {
   const element = document.createElement("h1");
 
-  // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = "teste";
 
   return element;
