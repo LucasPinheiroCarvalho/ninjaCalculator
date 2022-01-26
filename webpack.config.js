42b75224620bfc31fd06b8b937fdbb041d2487d6
LucasPinheiroCarvalho <lucas.pinho.31@gmail.com>
2022-01-26 17:54:28 -0300

    :art: add configs and dependecies Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,24 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?ts$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          },
+        },
+      },
+    ],
   },
 };
