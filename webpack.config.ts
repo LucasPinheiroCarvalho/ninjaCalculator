0f7acd0aff112ca4c84cd07af27769d30e78db5b
Victor Neves <victor.neves4@fatec.sp.gov.br>
2022-02-15 20:56:14 -0300

    :wrench: add and update config files  On branch main  Changes to be committed: 	new file:   .babelrc 	modified:   tsconfig.json 	new file:   webpack.config.ts

---
+++ webpack.config.ts
@@ -0,0 +1,45 @@
+import path from "path";
+
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
+
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 4000,
+  },
+};
+
+export default config;
